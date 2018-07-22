46c1f8e238b7872514cb38c2863178625ac7c5d9
David Oti <davmixcool@gmail.com>
2018-07-22 11:27:14 +0100

    Format and add support for @transformer response
    Add Transformer response
    Format explicitly defined response in the doc block

--- src/Oxycoder/ApiDoc/Generators/DingoGenerator.php
+++ src/Oxycoder/ApiDoc/Generators/DingoGenerator.php
@@ -4,6 +4,12 @@ namespace Oxycoder\ApiDoc\Generators;
 
 use Exception;
 
+use League\Fractal\Manager;
+use League\Fractal\Resource\Item;
+use Oxycoder\ApiDoc\Reflection\DocBlock\Tag;
+use League\Fractal\Resource\Collection;
+use ReflectionClass;
+
 class DingoGenerator extends AbstractGenerator
 {
     /**
@@ -15,19 +21,45 @@ class DingoGenerator extends AbstractGenerator
      * @return array
      */
     public function processRoute($route, $bindings = [], $headers = [], $withResponse = true)
-    {
-        $response = '';
-
-        if ($withResponse) {
-            try {
-                $response = $this->getRouteResponse($route, $bindings, $headers);
-            } catch (Exception $e) {
-            }
-        }
+     {
+        $content = '';
 
         $routeAction = $route->getAction();
         $routeGroup = $this->getRouteGroup($routeAction['uses']);
         $routeDescription = $this->getRouteDescription($routeAction['uses']);
+        $showresponse = null;
+
+        if ($withResponse) {
+            $response = null;
+            $docblockResponse = $this->getDocblockResponse($routeDescription['tags']);
+
+            if ($docblockResponse) {
+                // we have a response from the docblock ( @response )
+                $response = json_decode($docblockResponse->getContent());
+                $showresponse = true;
+            }
+            if (! $response) {
+                $transformerResponse = $this->getTransformerResponse($routeDescription['tags']);
+                if ($transformerResponse) {
+                    // we have a transformer response from the docblock ( @transformer || @transformercollection )
+                    $response = json_decode($transformerResponse->getContent());
+                    $showresponse = true;
+                }
+            }
+            if (! $response) {
+                try {
+                    $response = $this->getRouteResponse($route, $bindings, $headers);
+                    if (is_object($response)) {
+                        $response = json_encode(json_decode($response->getContent(), JSON_PRETTY_PRINT));
+                    }
+                } catch (Exception $e) {
+                }
+            }else{
+                //Formart explicietly defined response in the docblock
+                $response = json_decode( $response, JSON_PRETTY_PRINT); 
+            }
+            $content = $response;
+        }
 
         return $this->getParameters([
             'id' => md5($route->uri().':'.implode($route->getMethods())),
@@ -37,9 +69,106 @@ class DingoGenerator extends AbstractGenerator
             'methods' => $route->getMethods(),
             'uri' => $route->uri(),
             'parameters' => [],
-            'response' => $response,
+            'response' => $content,
+            'showresponse' => $showresponse,
         ], $routeAction, $bindings);
     }
+
+
+     /**
+     * Get a response from the transformer tags.
+     *
+     * @param array $tags
+     *
+     * @return mixed
+     */
+    protected function getTransformerResponse($tags)
+    {
+        try {
+            $transFormerTags = array_filter($tags, function ($tag) {
+                if (! ($tag instanceof Tag)) {
+                    return false;
+                }
+
+                return \in_array(\strtolower($tag->getName()), ['transformer', 'transformercollection']);
+            });
+            if (empty($transFormerTags)) {
+                // we didn't have any of the tags so goodbye
+                return false;
+            }
+
+            $modelTag = array_first(array_filter($tags, function ($tag) {
+                if (! ($tag instanceof Tag)) {
+                    return false;
+                }
+
+                return \in_array(\strtolower($tag->getName()), ['transformermodel']);
+            }));
+            $tag = \array_first($transFormerTags);
+            $transformer = $tag->getContent();
+            if (! \class_exists($transformer)) {
+                // if we can't find the transformer we can't generate a response
+                return;
+            }
+            $demoData = [];
+
+            $reflection = new ReflectionClass($transformer);
+            $method = $reflection->getMethod('transform');
+            $parameter = \array_first($method->getParameters());
+            $type = null;
+            if ($modelTag) {
+                $type = $modelTag->getContent();
+            }
+            if (version_compare(PHP_VERSION, '7.0.0') >= 0 && \is_null($type)) {
+                // we can only get the type with reflection for PHP 7
+                if ($parameter->hasType() &&
+                ! $parameter->getType()->isBuiltin() &&
+                \class_exists((string) $parameter->getType())) {
+                    //we have a type
+                    $type = (string) $parameter->getType();
+                }
+            }
+            if ($type) {
+                // we have a class so we try to create an instance
+                $demoData = new $type;
+                try {
+                    // try a factory
+                    $demoData = \factory($type)->make();
+                } catch (\Exception $e) {
+                    if ($demoData instanceof \Illuminate\Database\Eloquent\Model) {
+                        // we can't use a factory but can try to get one from the database
+                        try {
+                            // check if we can find one
+                            $newDemoData = $type::first();
+                            if ($newDemoData) {
+                                $demoData = $newDemoData;
+                            }
+                        } catch (\Exception $e) {
+                            // do nothing
+                        }
+                    }
+                }
+            }
+
+            $fractal = new Manager();
+            $resource = [];
+            if ($tag->getName() == 'transformer') {
+                // just one
+                $resource = new Item($demoData, new $transformer);
+            }
+            if ($tag->getName() == 'transformercollection') {
+                // a collection
+                $resource = new Collection([$demoData, $demoData], new $transformer);
+            }
+
+            return \response($fractal->createData($resource)->toJson());
+        } catch (\Exception $e) {
+            // it isn't possible to parse the transformer
+            return;
+        }
+    }
+
+
 
     /**
      * Prepares / Disables route middlewares.
